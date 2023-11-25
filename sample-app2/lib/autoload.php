32dd026a53e2a451ff0a120f5be7e46b67ebbcf5
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-11-26 01:16:00 +0900

    sample-app2のautoloadにデバッグコードを追加

--- sample-app2/lib/autoload.php
+++ sample-app2/lib/autoload.php
@@ -1,5 +1,6 @@
 <?php
 spl_autoload_register(function ($class_name) {
+    print 'spl_autoload_register: ' . $class_name  . 'をインクルードしてきます' . PHP_EOL;
     include __DIR__ . '/../src/' .$class_name . '.php';
-    print 'spl_autoload_register: ' . $class_name . PHP_EOL;
+    print 'spl_autoload_register: ' . $class_name  . 'をインクルードしてきました' . PHP_EOL;
 });
